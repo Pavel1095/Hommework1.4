e2e7d068afab7ae14d0b1472ca4a35bb84cd368b
Pavel1095 <9153220501@mail.ru>
2022-07-23 17:47:36 +0300

    Задание 3 выполнено.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-    //  Задание 2
-        int firstFriday = 5;
-        for (int currentFriday = firstFriday; currentFriday <= 31; currentFriday += 7) {
-            System.out.println("Сегодня пятница " + currentFriday + "-е число. Необходимо подготовить отчет.");
+    //  Задание 3
+        int currentYear = 2022;
+
+        int start = currentYear - 200;
+        int end = currentYear + 100;
+
+        for (int year = start;year < end; year ++) {
+            if(year % 79 == 0) {
+                System.out.println(year);
+            }
         }
     }
 }
